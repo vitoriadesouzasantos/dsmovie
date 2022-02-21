cfa0911e070efa0a9b648ffb1b44e91034351f13
vitoriadesouzasantos <vitoria.santos@bandtec.com.br>
2022-02-21 14:28:48 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -1,2 +1,42 @@
-package com.devsuperior.dsmovie.entities;public class Score {
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+    @EmbeddedId
+    private ScorePK id;
+    private Double value;
+
+    public Score() {
+        this.id = new ScorePK();
+    }
+
+    public void setMovie(Movie movie) {
+        this.id.setMovie(movie);
+    }
+
+    public void setUser(User user) {
+        this.id.setUser(user);
+    }
+
+    public ScorePK getId() {
+        return id;
+    }
+
+    public void setId(ScorePK id) {
+        this.id = id;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
 }
