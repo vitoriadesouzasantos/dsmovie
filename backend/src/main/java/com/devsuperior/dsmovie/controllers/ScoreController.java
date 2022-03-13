78269c7d9decde0eac90e55aa553398562fdc25e
vitoriadesouzasantos <vitoria.santos@bandtec.com.br>
2022-03-13 20:30:07 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -2,11 +2,8 @@ package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.dto.ScoreDTO;
-import com.devsuperior.dsmovie.services.MovieService;
 import com.devsuperior.dsmovie.services.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
