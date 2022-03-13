4bbd6c080be24dedd45bda5130270eb9f536bc3b
vitoriadesouzasantos <vitoria.santos@bandtec.com.br>
2022-03-13 19:36:24 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -1,27 +1,25 @@
 package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
 import com.devsuperior.dsmovie.services.MovieService;
+import com.devsuperior.dsmovie.services.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/movies")
-public class MovieController {
+@RequestMapping(value = "/scores")
+public class ScoreController {
 
     @Autowired
-    private MovieService service;
+    private ScoreService service;
 
-    @GetMapping
-    public Page<MovieDTO> findAll(Pageable pageable) {
-        return service.findAll(pageable);
-    }
-
-    @GetMapping(value ="/{id}")
-    public MovieDTO findById(@PathVariable Long id) {
-        return service.findById(id);
+    @PutMapping
+    public MovieDTO saveScore(@RequestBody ScoreDTO dto) {
+        MovieDTO movieDTO = service.saveScore(dto);
+        return movieDTO;
     }
 
 }
