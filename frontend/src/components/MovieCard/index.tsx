f17628963d9eeab3d98b9b616943de5a7a392755
vitoriadesouzasantos <vitoria.santos@bandtec.com.br>
2022-03-14 01:43:15 -0300

    Save, useNavigate

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -6,18 +6,19 @@ type Props = {
     movie: Movie;
 }
 
-export default function MovieCard({ movie } : Props) {
+export default function MovieCard({ movie }: Props) {
 
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore count={movie.count} score={movie.score}/>
+                <MovieScore count={movie.count} score={movie.score} />
 
-                <Link to={`/form/${movie.id}`}>
+                <Link to={`form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
+
             </div>
         </div>
     );
